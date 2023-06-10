56be74539512bbde9a3e0ecd42498eb0ac6a1784
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 17:38:32 +0530

    list or array operations

--- bin/basics_program.dart
+++ bin/basics_program.dart
@@ -4,9 +4,19 @@ void main() {
   List<int> numberlist = [12, 34, 56, 75]; // fixed length list
   var array = [12, 'sudo', 82.25, 2]; // dynamic array
   print(numberlist.length);
+  var listToadd = [1, 2, 3];
   if (numberlist.contains(11)) {
     print('no. is in the list');
   } else {
     print('no. is not in the list');
   }
+  numberlist.add(21); // add element at the end of the list
+  numberlist.insert(1, 22); // add element at the given index
+  numberlist.remove(12); // remove element from the list
+  numberlist.removeAt(2); // remove element from the given index
+  numberlist.removeLast(); // remove last element from the list
+  numberlist.addAll(listToadd); // add all the elements of the list
+  print(
+      numberlist.join('*')); // join the elements of the list with given symbol
+  print(numberlist.reversed); // reverse the list
 }
