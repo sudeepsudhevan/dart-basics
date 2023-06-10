3d7be1d31ae2c6920c850f54c4960c179338c61d
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 16:28:04 +0530

    for loop

--- bin/basics_program.dart
+++ bin/basics_program.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
 void main() {
-  print('Enter a number');
-  var number = int.parse(stdin.readLineSync()!);
+  // print('Enter a number');
+  // var number = int.parse(stdin.readLineSync()!);
 
-  if (number > 0) {
-    print("number is positive");
-  } else if (number == 0) {
-    print("number is zero");
-  } else {
-    print("number is negative");
+  // if (number > 0) {
+  //   print("$number is positive");
+  // } else if (number == 0) {
+  //   print("$number is zero");
+  // } else {
+  //   print("$number is negative");
+  // }
+  for (var i = 0; i < 10; i++) {
+    //for loop
+    print(i);
   }
 }
