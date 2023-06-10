d8a1fe48d5e876b64a028cfc6873d9c74b1409c3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 15:40:44 +0530

    add two number

--- bin/basics_program.dart
+++ bin/basics_program.dart
@@ -1,7 +1,18 @@
 import 'dart:io';
 
 void main() {
-  print('Hello Wrld');
-  var a = stdin.readLineSync();
-  print('Hello $a');
+  // print('Hello Wrld');
+  // var a = stdin.readLineSync();
+  // print('Hello $a');
+  print('Enter 2 numbers');
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
+  if (input1 == null) {
+    print('Enter a valid number');
+    return;
+  }
+  var num1 = int.parse(input1);
+  var num2 =
+      int.parse(input2!); // ! is used to tell compiler that it is not null
+  print('Sum of $num1 and $num2 is ${num1 + num2}');
 }
