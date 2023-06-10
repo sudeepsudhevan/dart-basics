247b18f8b6fda8fe80aa65b9cdddd2b87c600636
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 15:20:42 +0530

    dart basics

---
+++ bin/basics_program.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  print('Hello Wrld');
+  var a = stdin.readLineSync();
+  print('Hello $a');
+}
---
+++ test/basics_program_test.dart
@@ -0,0 +1,8 @@
+import 'package:basics_program/basics_program.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
