6aa0980414c2f5c536ce6a23594ef3e4c9508af6
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 16:01:54 +0530

    data types

--- bin/basics_program.dart
+++ bin/basics_program.dart
@@ -4,15 +4,24 @@ void main() {
   // print('Hello Wrld');
   // var a = stdin.readLineSync();
   // print('Hello $a');
-  print('Enter 2 numbers');
-  var input1 = stdin.readLineSync();
-  var input2 = stdin.readLineSync();
-  if (input1 == null) {
-    print('Enter a valid number');
-    return;
-  }
-  var num1 = int.parse(input1);
-  var num2 =
-      int.parse(input2!); // ! is used to tell compiler that it is not null
-  print('Sum of $num1 and $num2 is ${num1 + num2}');
+  // print('Enter 2 numbers');
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+
+  // var num1 = int.parse(input1!);
+  // var num2 =
+  //     int.parse(input2!); // ! is used to tell compiler that it is not null
+  // print('Sum of $num1 and $num2 is ${num1 + num2}');
+  int a = 10;
+  double b = 10.5; // double is used for decimal values
+  num num1 = 10; // num is used for both int and double
+  num num2 = 10.5;
+  String name = 'sudeep';
+  String name1 =
+      "sudeep i' am 10 escape sequance\" years old"; // string can be written in both single and double quotes
+  String address = '''sudhu is
+well it is
+know for multi line''';
+  print(name.length);
+  print(address.length);
 }
