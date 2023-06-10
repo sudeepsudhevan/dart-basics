8af3fc493ada207419dcec85de759e9e83103589
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-10 16:20:14 +0530

    positive negative and zero if else if

--- bin/basics_program.dart
+++ bin/basics_program.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 void main() {
-  int number = 10;
+  print('Enter a number');
+  var number = int.parse(stdin.readLineSync()!);
+
   if (number > 0) {
     print("number is positive");
+  } else if (number == 0) {
+    print("number is zero");
   } else {
     print("number is negative");
   }
